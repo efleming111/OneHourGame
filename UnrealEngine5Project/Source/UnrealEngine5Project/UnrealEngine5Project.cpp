fb1ceb182357dc56bc567cf3dd0a93988edf1401
efleming111 <efleming111@yahoo.com>
2024-08-29 16:30:14 -0700

    Create new Unreal project and added Unity project

--- UnrealEngine5Project/Source/UnrealEngine5Project/UnrealEngine5Project.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UnrealEngine5Project.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealEngine5Project, "UnrealEngine5Project" );
